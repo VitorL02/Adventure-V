1d84f9b8fdb21ff9e24b6767c2f5a2fbb964d78c
Vitor Lucas Crispim <68614036+VitorL02@users.noreply.github.com>
2020-07-22 15:46:43 -0300

    Add files via upload

---
+++ Codigos- Code (Separated)/BatIA.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatIA : MonoBehaviour {
+
+	private GameController _GameController;
+	private bool isseguir;
+	public float speed;
+	public bool olhandoesquerda;
+	public GameObject hitboxMr;
+	private Rigidbody2D morcegoRB;
+	private Animator MorcegoAnimator;
+	void Start () {
+		
+		_GameController = FindObjectOfType (typeof(GameController))as GameController;
+		morcegoRB = GetComponent<Rigidbody2D> ();
+		MorcegoAnimator = GetComponent<Animator>();
+
+	}
+
+
+	void Update () {
+		if (_GameController.currentState != gameState.GAMEPLAY) {
+			return;
+		}
+		if (isseguir == true) {
+			transform.position = Vector3.MoveTowards(transform.position,_GameController.playerTransform.position,speed*Time.deltaTime); 
+		}
+		if (transform.position.x < _GameController.playerTransform.position.x && olhandoesquerda == true) {
+			Vira ();
+		} else if (transform.position.x > _GameController.playerTransform.position.x && olhandoesquerda == false)
+			Vira ();
+		
+	}
+	void OnBecameVisible(){
+		isseguir = true;
+
+	}
+	void Vira(){
+
+		olhandoesquerda = !olhandoesquerda; //! = CONTRARIO, retorna a variavavel a falso
+		//FLOAT PRA SABER DIFERENÇA  de direção
+		float x = transform.localScale.x * -1;
+		transform.localScale= new Vector3(x,transform.localScale.y,transform.localScale.z);   // Retorna Pra posição inicial
+
+	}
+	void  OnTriggerEnter2D(Collider2D col){
+
+		if (col.gameObject.tag == "HitBox") {
+		Destroy (hitboxMr);
+		_GameController.playSfx (_GameController.sfxMortInim, 0.03f);
+		MorcegoAnimator.SetTrigger ("Morte");
+	}
+	} 
+
+void OnDead (){
+			Destroy(this.gameObject);
+		}
+
+
+}
---
+++ Codigos- Code (Separated)/ParallaxControl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxControl : MonoBehaviour {
+
+
+	public Transform   background;
+	public float       speed;
+
+	private Transform cam;
+	private  Vector3 previewCamPosition;
+
+	// Use this for initialization
+	void Start () {
+
+		cam = Camera.main.transform;
+		previewCamPosition = cam.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float parallaxx = previewCamPosition.x - cam.position.x;
+		float bgTargetX = background.position.x + parallaxx ;
+		Vector3 bgPosition = new Vector3 (  bgTargetX,background.position.y,background.position.z);
+		background.position = Vector3.Lerp (background.position, bgPosition, speed * Time.deltaTime);
+
+		previewCamPosition = cam.position;
+		
+	}
+}
